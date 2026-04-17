a39c8251f8b3859bce08b22cf2cdf92fc1b9354d
Ash-x1 <ash91p@proton.me>
2026-04-17 19:05:54 +0100

    Create partial form for new and edit pages and add validations in kitten model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,5 @@
 class Kitten < ApplicationRecord
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cuteness, numericality: true
+  validates :softness, numericality: true
 end
